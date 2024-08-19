616d4a2ceac4fc100efd7c12ab62b344166f8e91
agmt92 <155798390+agmt92@users.noreply.github.com>
2024-08-19 13:51:10 +0200

    fix: add dependencies in package.json, fix basic configs dirs in index.js

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+
 app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(`${process.cwd()}/index.html`);
 });
 
 // Connect to MongoDB
